76165091dcd1b7c792e137ee5ccd34b1a724a81a
Senhorinha Joana <senhorinha.alves@ufpe.br>
2024-09-13 17:33:05 -0300

    Modularizando as funções do sistema

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -11,3 +11,35 @@ limite = 500
 extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
+
+def depositar(saldo, extrato):
+    valor = float(input("Informe o valor do depósito: "))
+    if valor > 0:
+        saldo += valor
+        extrato += f"Depósito: R$ {valor:.2f}\n"
+    else:
+        print("Operação falhou! O valor informado é inválido.")
+    return saldo, extrato
+
+def sacar(saldo, extrato, numero_saques):
+    valor = float(input("Informe o valor do saque: "))
+    if valor <= 0:
+        print("Operação falhou! O valor informado é inválido.")
+    elif valor > saldo:
+        print("Operação falhou! Você não tem saldo suficiente.")
+    elif valor > limite:
+        print("Operação falhou! O valor do saque excede o limite.")
+    elif numero_saques >= LIMITE_SAQUES:
+        print("Operação falhou! Número máximo de saques excedido.")
+    else:
+        saldo -= valor
+        extrato += f"Saque: R$ {valor:.2f}\n"
+        numero_saques += 1
+    return saldo, extrato, numero_saques
+
+def exibir_extrato(saldo, extrato):
+    print("\n================ EXTRATO ================")
+    print("Não foram realizadas movimentações." if not extrato else extrato)
+    print(f"\nSaldo: R$ {saldo:.2f}")
+    print("==========================================")
+
